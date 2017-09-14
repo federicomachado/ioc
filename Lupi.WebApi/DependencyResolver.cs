f964961fbeb0aff8235cfe1ed340f813ac86d0d6
Federico Machado <federicomachado@outlook.com>
2017-09-14 20:11:27 -0300

    More things

---
+++ Lupi.WebApi/DependencyResolver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using Lupi.BusinessLogic;
+using System.ComponentModel;
+
+namespace Lupi.WebApi
+{
+    [Export(typeof)(IComponent)]
+    class DependencyResolver : IComponent
+    {
+
+        public void SetUp(IRegisterComponent registerComponent)
+        {
+            registerComponent.RegisterType<IBreedBusinessLogic, BreedBusinessLogic>();
+        }
+    }
+}
